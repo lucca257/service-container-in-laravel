231d5d96db440de103f85d6437ccbae0d32e7f8d
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-04 13:53:55 -0300

    utilizando AppServiceProvider para registrar currency ao ser criado o objeto

--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -8,11 +8,18 @@ use Illuminate\Support\Str;
 
 class PaymentGateway
 {
+    private $currency;
+    public function __construct($currency)
+    {
+        $this->currency = $currency;
+    }
+
     public function charge($amount){
         //charge the bank
         return [
             'amount' => $amount,
             'confirmation_number' => Str::random(),
+            'currency' => $this->currency
         ];
     }
 }
