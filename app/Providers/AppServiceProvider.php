253a5bd88404fa875bba309b9adc8a2d0a1bd339
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-04 14:37:21 -0300

    adicionando nova forma de pagmento e servico de forma dinamica

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
 use App\Billing\PaymentGatewayContract;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(PaymentGatewayContract::class, function($app){
+            if(request()->has('credit')) {
+                return new CreditPaymentGateway('usd');
+            }
             return new BankPaymentGateway('usd');
         });
     }
